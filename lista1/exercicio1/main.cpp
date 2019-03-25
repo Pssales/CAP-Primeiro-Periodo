afd6bdda74b262fdefc4b31819fb287e0b1efc9e
Pssales <camilapsales27@gmail.com>
2019-03-25 11:28:00 -0300

    Alteração pilha
    mudar a alocação de memória

--- lista1/exercicio1/main.cpp
+++ lista1/exercicio1/main.cpp
@@ -1,3 +1,15 @@
+/*Projete e implemente uma estrutura de dados do tipo pilha que
+permita empilhar e desempilhar números inteiros. Esta estrutura deverá fornecer as
+seguintes operações:
+• push(v): Empilha um número inteiro.
+• pop(): Desempilha o elemento no topo da pilha.
+• top(): Retorna o valor do elemento no topo da pilha.
+• empty(): Retorna verdadeiro se a pilha estiver vazia, caso contrário, retorna falso.
+Além das operações acima, os clientes da pilha poderão criar objetos com uma determinada capacidade inicial de armazenamento (ex: 4 elementos). No entanto, durante as
+operações de empilhamento (push), caso haja a necessidade de alocação de mais memória,
+a implementação deverá cuidar desses detalhes
+*/
+
 // MyCode
 #include "pilha.hpp"
  
@@ -8,7 +20,14 @@ int main(int argc, char *argv[])
 {
 	 
     Pilha p;
-
+    std::cout << "vazia:" << p.empty() << std::endl;
+    p.push(10);
+    p.push(20);
+    p.push(30);
+    p.push(40);
+    std::cout << "topo:" << p.top() << std::endl;
+    p.pop();
+    std::cout << "topo:" << p.top() << std::endl;
     std::cout << "vazia:" << p.empty() << std::endl;
     
     return 0;
--- lista1/exercicio1/pilha.hpp
+++ lista1/exercicio1/pilha.hpp
@@ -1,14 +1,3 @@
-/*Projete e implemente uma estrutura de dados do tipo pilha que
-permita empilhar e desempilhar números inteiros. Esta estrutura deverá fornecer as
-seguintes operações:
-• push(v): Empilha um número inteiro.
-• pop(): Desempilha o elemento no topo da pilha.
-• top(): Retorna o valor do elemento no topo da pilha.
-• empty(): Retorna verdadeiro se a pilha estiver vazia, caso contrário, retorna falso.
-Além das operações acima, os clientes da pilha poderão criar objetos com uma determinada capacidade inicial de armazenamento (ex: 4 elementos). No entanto, durante as
-operações de empilhamento (push), caso haja a necessidade de alocação de mais memória,
-a implementação deverá cuidar desses detalhes
-*/
 #ifndef __PILHA_HPP__
 #define __PILHA_HPP__
 
