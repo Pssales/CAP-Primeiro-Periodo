83891543a413449fcdd4767b454a7582af45e342
Pssales <camilapsales27@gmail.com>
2019-03-25 11:40:21 -0300

    descrição exer 2

---
+++ lista1/exercicio2/main.cpp
@@ -0,0 +1,11 @@
+/*
+
+Projete e implemente uma estrutura de dados do tipo lista duplamente encadeada que forneça as seguintes operações:
+• front(): Retorna o primeiro elemento da lista.
+• back(): Retorna o último elemento da lista.
+• push_front(v): Insere o valor v na cabeça da lista.
+• push_back(v): Insere o valor v ao final da lista.
+• pop_front(): Remove o primeiro elemento da lista.
+• pop_back(): Remove o último elemento da lista
+
+*/
