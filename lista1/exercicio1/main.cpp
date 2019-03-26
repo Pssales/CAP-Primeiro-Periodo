20fb93eea46bf901bfedac80aa13469587dbaa60
Camila_Sales <camilapsales27@gmail.com>
2019-03-26 20:17:30 -0300

    adição de menu

--- lista1/exercicio1/main.cpp
+++ lista1/exercicio1/main.cpp
@@ -20,16 +20,39 @@ int main(int argc, char *argv[])
 {
 	 
     Pilha p;
-    std::cout << "vazia:" << p.empty() << std::endl;
-    p.push(10);
-    p.push(20);
-    p.push(30);
-    p.push(40);
-    std::cout << "topo:" << p.top() << std::endl;
-    p.pop();
-    std::cout << "topo:" << p.top() << std::endl;
-    std::cout << "vazia:" << p.empty() << std::endl;
-    
+
+    int option;
+    do
+    {
+        std::cout << "------------------" << std::endl;
+        std::cout << "Escolha uma das opções:" << std::endl;
+        std::cout << "1 - Empilhar" << std::endl;
+        std::cout << "2 - Remover topo" << std::endl;
+        std::cout << "3 - Exibir topo" << std::endl;
+        std::cout << "4 - Verificar se esta vazia" << std::endl;
+        std::cout << "0 - Encerar o programa" << std::endl;
+        std::cin >> option ;
+
+        switch (option){
+            case 1:
+                int value;
+                std::cout << "Informe o número" << std::endl;
+                std::cin >> value ;
+                p.push(value);
+            break;
+            case 2:
+                p.pop();
+            break;
+            case 3:
+                std::cout << p.top() << std::endl;
+            break;
+            case 4:
+                std::cout << p.empty() << std::endl;
+            break;
+        }
+        
+    } while (option != 0);
+   
     return 0;
 
 }
