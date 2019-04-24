c9754596c00075a7449df77e9dc8aab8b8fddc30
Camila Sales <camilapsales27@gmail.com>
2019-04-24 10:58:54 -0300

    Add changes

--- ComputationalMathematics/exercise2/one.py
+++ ComputationalMathematics/exercise2/one.py
@@ -2,43 +2,50 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
+# def uniforme(x):
 
 def binomial(x):
     n = 10
     p = 0.3
     binomial = stats.binom.pmf(x, n,p)
     print(binomial)
-    plt.plot(x,binomial)
+    plt.plot(x,binomial, "k--")
     plt.show()
 
 
-# def uniforme(results):
 
 def beta(x):
     a= 0.5
     b=0.5
     y = stats.beta.pdf(x,a,b)
     print(y)
-    plt.plot(x,y)
+    plt.plot(x,y,"k--")
     plt.show()
-# def laplace(results):
+# def laplace(x):
 
-# def gamma(results):
+# def gamma(x):
 
-# def expoencial(results):
+def exponencial(x):
+    lamb = 0.5
+    y = lamb * np.exp(-lamb *x)
 
-# def qui_quadrado(results):
+    plt.plot(x,y, "k--")
+    plt.show()
+# def qui_quadrado(x):
 
-# def cauchy(results):
+# def cauchy(x):
+
 
 def gaussiana(x):
     y = stats.norm.pdf(x,0,1)
     print(y)
-    plt.plot(x,y)
+    plt.plot(x,y,"k--")
     plt.show()
 
 
 gaussiana(np.arange(0.01,1,0.01))
 beta(np.arange(0.01,1,0.01))
 binomial(np.arange(0,21))
+exponencial(np.arange(0,15,0.1))
+cauchy(np.arange(0,15,0.1))
 
