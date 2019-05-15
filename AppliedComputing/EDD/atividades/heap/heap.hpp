2afe41e64429061887098eedef7ae0dde69b0967
Pssales <camilapsales27@gmail.com>
2019-05-15 14:33:23 -0300

    Finalizado heap

--- AppliedComputing/EDD/atividades/heap/heap.hpp
+++ AppliedComputing/EDD/atividades/heap/heap.hpp
@@ -1,18 +1,17 @@
-#ifndef __Heap__HPP__
-#define __Heap__HPP__
-
+#ifndef __HEAP__HPP__
+#define __HEAP__HPP__
 
 #include <iostream>
-#include <vector>
+#include <vector> 
 
 using namespace std;
 
-template <class T>
+template <class Template>
 class Heap {
     public:
         Heap();
 
-        void inserir(T );
+        void inserir(Template );
         void remover();
 
     //  Métodos de manipulação
@@ -25,9 +24,11 @@ class Heap {
         int getPai(int i);
         int filhoDireita(int i);
         int filhoEsquerda(int i);
+        int getFilhoDireita(int i);
+        int getFilhoEsquerda(int i);
 
     // Retorna o valor presente na raiz   
-        T root();
+        Template root();
         
     // Retorna a quantidade nós atual da arvore
         int tamanho();
@@ -36,7 +37,7 @@ class Heap {
     // Atribui o valor de capacidade
         void setCapacidade(int& n);
         
-        vector<T> elementos_;
+        vector<Template> elementos_;
         int capacidade_;
         
     
@@ -47,117 +48,139 @@ class Heap {
 
 };
 
-template <class T>
-Heap<T> :: Heap(){
+template <class Template>
+Heap<Template>::Heap(){
 }
 
 
-
-template <class T>
-void Heap<T> :: inserir(T value) {
-  elementos_.push_back(value);
-  moveUp();
+template <class Template>
+void Heap<Template>::inserir(Template valor) {
+    elementos_.push_back(valor);
+    moveUp();
 }
 
-template <class T>
-void Heap<T> :: remover() {
+template <class Template>
+void Heap<Template>::remover() {
 
-  if(elementos_.size() != 0){
-    int filho = elementos_.size()  - 1;
-    troca(filho, 0);
+    if(elementos_.size() != 0){
+        int filho = elementos_.size()  - 1;
+        troca(filho, 0);
+        
+        Template valor = elementos_.back();
+        elementos_.pop_back();
+        
+        moveDown();
+    }else{
+        cout << "Nao existem mais valores para serem removidos."<< endl;
+    }
+
+}
+
+
+template <class Template>
+void Heap<Template>::moveUp() {
+    int filho = elementos_.size() - 1;
+    int pai = getPai(filho);
     
-    T value = elementos_.back();
-    elementos_.pop_back();
-    
-    moveDown();
-  }else{
-      cout << "Nao existem mais valores para serem removidos."<< endl;
-  }
-
-}
-
-template <class T>
-void Heap <T>:: moveUp() {
-  int filho = elementos_.size() - 1;
-  int pai = getPai(filho);
-  
-  while (elementos_[filho] > elementos_[pai] && filho >=0 && pai >= 0) {
-	
-	troca(filho, pai);
-	filho = pai;
-	pai = getPai(filho);
-  }
+    while (elementos_[filho] > elementos_[pai] && filho >=0 && pai >= 0) {
+        troca(filho, pai);
+        filho = pai;
+        pai = getPai(filho);
+    }
 }
 
 
-template <class T>
-void Heap<T> :: moveDown() {
-  int pai = 0;
+template <class Template>
+void Heap<Template>::moveDown() {
+    int pai = 0;
 
-  while (true) {
-	int esquerda = filhoEsquerda(pai);
-	int direira = filhoDireita(pai);
-	int tamanho = elementos_.size();
-	int maior = pai;
+    while (true) {
+        int esquerda = filhoEsquerda(pai);
+        int direira = filhoDireita(pai);
+        int tamanho = elementos_.size();
+        int maior = pai;
 
-	if (esquerda < tamanho && elementos_[esquerda] > elementos_[maior])
-	  maior = esquerda;
+        if (esquerda < tamanho && elementos_[esquerda] > elementos_[maior])
+            maior = esquerda;
 
-	if (direira < tamanho && elementos_[direira] > elementos_[maior])
-	  maior = direira;
+        if (direira < tamanho && elementos_[direira] > elementos_[maior])
+            maior = direira;
 
-	if (maior != pai) {
-	  troca(maior, pai);
-	  pai = maior;
-	}
-	else 
-	  break;
-  }
+        if (maior != pai) {
+            troca(maior, pai);
+            pai = maior;
+        }else{
+            break;
+        }
+
+    }
 }
 
-template <class T>
-void Heap<T>::troca(int filho, int pai) {
-  T temp;
-  temp = elementos_[filho];
-  elementos_[filho] = elementos_[pai];
-  elementos_[pai] = temp;
+template <class Template>
+void Heap<Template>::troca(int filho, int pai) {
+    Template temp;
+    temp = elementos_[filho];
+    elementos_[filho] = elementos_[pai];
+    elementos_[pai] = temp;
 }
 
 
-template <class T>
-int Heap<T> :: getPai(int filho) {
+template <class Template>
+int Heap<Template>::getPai(int filho) {
     return (filho % 2 == 0)?(filho /2 ) -1 : filho/2  ;
 }
 
-template <class T>
-int Heap<T> :: filhoDireita(int pai){
-  return 2 * pai + 2;
+template <class Template>
+int Heap<Template>::filhoDireita(int pai){
+    return 2 * pai + 2;
 }
 
-template <class T>
-int Heap<T> :: filhoEsquerda(int pai){
-  return 2*pai +1;
+template <class Template>
+int Heap<Template>::filhoEsquerda(int pai){
+    return 2*pai +1;
+}
+
+template <class Template>
+int Heap<Template>::getFilhoDireita(int pai){
+    if (elementos_.size() > (2 * pai + 2))
+    {
+        return elementos_[2 * pai + 2];
+    }else{
+        cout << "Não existe filho a direita"<< endl;
+    }
+    
+}
+
+template <class Template>
+int Heap<Template>::getFilhoEsquerda(int pai){
+     if (elementos_.size() > (2 * pai + 2))
+    {
+        return elementos_[2*pai +1];
+    }else{
+        cout << "Não existe filho a esquerda"<< endl;
+    }
 }
 
 
-template <class T>
-T Heap<T> :: root() {
+template <class Template>
+Template Heap<Template>::root() {
 	return elementos_[0];
 }
 
 
-template <class T>
-int Heap<T>::tamanho(){
-  return elementos_.size();
+template <class Template>
+int Heap<Template>::tamanho(){
+    return elementos_.size();
 }
 
-template <class T>
-int Heap<T>::capacidade(){
-  return capacidade_;
+template <class Template>
+int Heap<Template>::capacidade(){
+    return capacidade_;
 }
-template <class T>
-void Heap<T>::setCapacidade(int& n){
-   capacidade_ = n;
+
+template <class Template>
+void Heap<Template>::setCapacidade(int& n){
+    capacidade_ = n;
 }
 
 
--- AppliedComputing/EDD/atividades/heap/main.cpp
+++ AppliedComputing/EDD/atividades/heap/main.cpp
@@ -3,38 +3,35 @@
 int main(){
 
     Heap<int> heap;
-
-    int a[] = {4, 5,2,3,6,7};
-    int len = sizeof(a) /sizeof(int);
     int valor;
+    int opcao;
     
 	cout << "Qual o numero de posicoes da arvore?" << endl;
     cin >> valor ;
     heap.setCapacidade(valor);
 
-    while (heap.capacidade() > heap.tamanho())
-    {
+    while (heap.capacidade() > heap.tamanho()){
         cout << "Insira um valor" << endl;
         cin >> valor ;
         heap.inserir(valor);
     }
     
 
-    int option;
-    do
-    {
+    do{
         cout << "------------------" << endl;
         cout << "Escolha uma das alternativas:" << endl;
         cout << "1 - Inserir" << endl;
         cout << "2 - Remover topo" << endl;
         cout << "3 - Exibir raiz" << endl;
         cout << "4 - Exibir pai" << endl;
-        cout << "5 - Exibir filho a direita" << endl;
-        cout << "6 - Exibir filho a esquerda" << endl;
+        cout << "5 - Exibir posicao do filho a direita" << endl;
+        cout << "6 - Exibir posicao do filho a esquerda" << endl;
+        cout << "7 - Exibir filho a direita" << endl;
+        cout << "8 - Exibir filho a esquerda" << endl;
         cout << "0 - Encerrar o programa" << endl;
-        cin >> option ;
+        cin >> opcao;
 
-        switch (option){
+        switch (opcao){
             case 1:
                 if(heap.capacidade() > heap.tamanho()){
                     cout << "Informe o valor" << endl;
@@ -53,26 +50,31 @@ int main(){
             case 4:
                 cout << "Digite a posicao do elemento que deseja exibir o elemento pai" << endl;
                 cin >> valor ;
-                heap.getPai(valor);
+                cout << "Posição do elemento pai"<< heap.getPai(valor) << endl;
+            
             break;
             case 5:
                 cout << "Digite a posicao do elemento que deseja exibir o elemento filho a direita" << endl;
                 cin >> valor ;
-                cout << heap.filhoDireita(valor);
+                cout << "Posicao do elemento filho a direita: "<< heap.filhoDireita(valor) << endl;
             break;
             case 6:
                 cout << "Digite a posicao do elemento que deseja exibir o elemento filho a esquerda" << endl;
                 cin >> valor ;
-                heap.filhoEsquerda(valor);
+                cout << "Posicao do elemento filho a esquerda: "<< heap.filhoEsquerda(valor) << endl;
+            case 7:
+                cout << "Digite a posicao do elemento que deseja exibir o elemento filho a direita" << endl;
+                cin >> valor ;
+                cout << "Elemento filho a direita: "<< heap.getFilhoDireita(valor) << endl;
+            break;
+            case 8:
+                cout << "Digite a posicao do elemento que deseja exibir o elemento filho a esquerda" << endl;
+                cin >> valor ;
+                cout << "Elemento filho a esquerda: "<< heap.getFilhoEsquerda(valor) << endl;
             break;
         }
         
-    } while (option != 0);
-
-    while(heap.tamanho() > 0){
-        heap.remover();
-        cout<<"Heap Max\t"<< heap.root()<<endl;
-    }
+    } while (opcao != 0);
 
     return 0;
 
