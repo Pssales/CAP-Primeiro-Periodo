81168b5d331b85605e9bc8c3fb4815f8641f5815
Pssales <camilapsales27@gmail.com>
2019-06-03 16:52:20 -0300

    Add

--- ComputationalMathematics/lista/exer9/exer9.py
+++ ComputationalMathematics/lista/exer9/exer9.py
@@ -1,24 +1,17 @@
+# import packages
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import waipy
 
-n, p = 10, .5 
-binomial = np.random.binomial(n, p, 1000)
-uniform = np.random.uniform(-1,0,1000)
-beta = np.random.beta(1, 4, 1000)
-laplace = np.random.laplace(0, 0.5, 1000)
-gamma = np.random.gamma(1, 2, 1000)
-exponential = np.random.exponential(1, 1000)
-quiquadrado = np.random.chisquare(1, 1000)
-cauchy = np.random.standard_cauchy(1000)
-gaussian = np.random.normal(0, 0.2, 1000)
-lognormal = np.random.lognormal(3, 1, 1000)
-sns.distplot(beta)
+nome = 'S8' # nome da série para plotagem
+# nomeArquivo = '..\s3.txt'		
+# nomeArquivo = '..\s7.txt'		
+nomeArquivo = '..\s8.txt'		
+data = np.genfromtxt(nomeArquivo, delimiter='\n', dtype = 'float32',filling_values = 0)
 
-sns.distplot(uniform, hist=False, color='red', label='Uniform')
-sns.distplot(binomial, hist=False, color='blue', label='Binomial')
-sns.distplot(beta, hist=False, color='blue', label='Beta')
-sns.distplot(laplace, hist=False, color='blue', label='Laplace')
-sns.distplot(gamma, hist=False, color='blue', label='Gamma')
-sns.distplot(exponential, hist=False, color='blue', label='Exponential')
-plt.show();
+time = np.linspace(0, len(data), len(data))
+
+data_norm = waipy.normalize(data)
+
+result = waipy.cwt(data_norm, dt=0.25, pad=1, dj=0.25, s0=2*0.25, j1=7/0.25, lag1=0.72, param=6, mother='Morlet', name=nome)
+
+waipy.wavelet_plot(var=nome, time=time, data=data_norm, dtmin=0.03125, result=result)
